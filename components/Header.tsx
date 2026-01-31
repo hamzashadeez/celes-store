5140abbb1e8ad416f72b59351f77c4ed44325dfd
hamzashadeez <hahmad1178@gmail.com>
2026-01-31 14:25:58 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,20 +11,19 @@ export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [user, setUser]: any = useState(null);
 
-  const getSignedUser = async () =>{
+  const getSignedUser = async () => {
     const user = await ApiClient.get("me")
-    .then((response)=>{
-      setUser(response.data?.user)
-    })
-    .catch((error)=>{
-      // console.log(error)
-    })
+      .then((response) => {
+        setUser(response.data?.user);
+      })
+      .catch((error) => {
+        // console.log(error)
+      });
+  };
 
-  }
-
-  useEffect(()=>{
-    getSignedUser()
-  },[])
+  useEffect(() => {
+    getSignedUser();
+  }, []);
 
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50 h-[70px]">
@@ -50,27 +49,30 @@ export function Header() {
           </form>
 
           {/* Desktop Actions */}
-        {user === null &&  <div className="hidden md:flex items-center gap-4">
-            <Link href="/auth/login">
-              <button className="text-gray-700 hover:text-purple-600 transition-colors flex items-center gap-2">
-                <User className="w-5 h-5" />
-                <span>Login</span>
-              </button>
-            </Link>
-            <Link href="/auth/register">
-              <button className="bg-gradient-to-r from-purple-600 to-pink-600 text-white px-6 py-2 rounded-full hover:shadow-lg transition-all">
-                Sign Up
-              </button>
-            </Link>
-          </div>}
-
-         {user && <div className="hidden md:flex  gap-3 items-center ">
-            <div className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full">
-              <User2 size={16} />
+          {user === null && (
+            <div className="hidden md:flex items-center gap-4">
+              <Link href="/auth/login">
+                <button className="text-gray-700 hover:text-purple-600 transition-colors flex items-center gap-2">
+                  <User className="w-5 h-5" />
+                  <span>Login</span>
+                </button>
+              </Link>
+              <Link href="/auth/register">
+                <button className="bg-gradient-to-r from-purple-600 to-pink-600 text-white px-6 py-2 rounded-full hover:shadow-lg transition-all">
+                  Sign Up
+                </button>
+              </Link>
             </div>
-            <h1 className="capitalize font-semibold ">{user?.name}</h1>
-          </div>
-}
+          )}
+
+          {user && (
+            <div className="hidden md:flex  gap-3 items-center ">
+              <div className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full">
+                <User2 size={16} />
+              </div>
+              <h1 className="capitalize font-semibold ">{user?.name}</h1>
+            </div>
+          )}
           {/* Mobile Menu Button */}
           <button
             className="md:hidden p-2"
@@ -95,29 +97,32 @@ export function Header() {
               </Button>
             </form>
             <nav className="flex flex-col gap-4">
-             
-          
-             {user === null && <div className="flex flex-col gap-2 pt-4 border-t border-gray-100">
-                <Link href={"/auth/login"}>
-                  <Button variant={'outline'} className="w-full text-blue-700">
-                    Login
-                  </Button>
-                </Link>
+              {user === null && (
+                <div className="flex flex-col gap-2 pt-4 border-t border-gray-100">
+                  <Link href={"/auth/login"}>
+                    <Button
+                      variant={"outline"}
+                      className="w-full text-blue-700"
+                    >
+                      Login
+                    </Button>
+                  </Link>
 
-                <Link href={"/auth/register"}>
-                  <Button variant={'outline'} className="w-full ">
-                    Sign Up
-                  </Button>
-                </Link>
-              </div>}
-                  {user && <div className="flex mt-3 gap-3 items-center ">
-            <div className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full">
-              <User2 size={16} />
-            </div>
-            <h1 className="capitalize font-semibold ">{user?.name}</h1>
-          </div>
-}
-
+                  <Link href={"/auth/register"}>
+                    <Button variant={"outline"} className="w-full ">
+                      Sign Up
+                    </Button>
+                  </Link>
+                </div>
+              )}
+              {user && (
+                <div className="flex mt-3 gap-3 items-center ">
+                  <div className="flex items-center justify-center w-10 h-10 bg-gray-200 rounded-full">
+                    <User2 size={16} />
+                  </div>
+                  <h1 className="capitalize font-semibold ">{user?.name}</h1>
+                </div>
+              )}
             </nav>
           </div>
         )}
