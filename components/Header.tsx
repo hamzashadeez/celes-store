e3c33b593b2d4b7678993ecbc503d14eeccd8858
hamzashadeez <hahmad1178@gmail.com>
2026-02-08 12:50:36 +0100

    done with hero

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,13 +28,12 @@ export function Header() {
   return (
     <header className="bg-[#14213D] shadow-sm sticky top-0 z-50 h-[70px]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-4">
+        <div className="flex justify-between items-center h-[70px] ">
           {/* Logo */}
-          <div className="flex items-center gap-2">
-            <div className="w-10 h-10 bg-gradient-to-br from-blue-600 to-indigo-400 rounded-xl flex items-center justify-center">
-              <Smartphone className="w-6 h-6 text-white" />
-            </div>
-            <span className="text-2xl bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
+          <div className="flex items-center gap-0">
+            
+            <img src="/logo.png" className="w-20" alt="" />
+            <span className="hidden md:block text-2xl bg-gradient-to-r from-[#FCA311] to-indigo-600 bg-clip-text text-transparent -ml-2">
               Celes Store
             </span>
           </div>
@@ -42,7 +41,7 @@ export function Header() {
           {/* Desktop Navigation */}
           <form className="gap-2 hidden md:flex">
             <Input required placeholder="Search Products..." />
-            <Button className="text-xs py-2 bg-indigo-600">
+            <Button className="text-xs py-2 bg-[#FCA311] text-black font-semibold">
               <Search />
               Search
             </Button>
@@ -58,9 +57,8 @@ export function Header() {
                 </button>
               </Link>
               <Link href="/auth/register">
-                <button className="bg-gradient-to-r from-purple-600 to-pink-600 text-white px-6 py-2 rounded-full hover:shadow-lg transition-all">
-                  Sign Up
-                </button>
+              
+                <Button>Sign Up</Button>
               </Link>
             </div>
           )}
@@ -79,9 +77,9 @@ export function Header() {
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             {isMenuOpen ? (
-              <X className="w-6 h-6 text-gray-700" />
+              <X className="w-6 h-6 text-gray-100" />
             ) : (
-              <Menu className="w-6 h-6 text-gray-700" />
+              <Menu className="w-6 h-6 text-gray-100" />
             )}
           </button>
         </div>
@@ -89,27 +87,21 @@ export function Header() {
         {/* Mobile Menu */}
         {isMenuOpen && (
           <div className="md:hidden py-4 border-t bg-white px-6 border-gray-100">
-            <form className="gap-2 flex flex-col">
-              <Input required placeholder="Search Products..." />
-              <Button className="text-xs py-2 bg-indigo-600">
-                <Search />
-                Search
-              </Button>
-            </form>
+            
             <nav className="flex flex-col gap-4">
               {user === null && (
                 <div className="flex flex-col gap-2 pt-4 border-t border-gray-100">
                   <Link href={"/auth/login"}>
                     <Button
                       variant={"outline"}
-                      className="w-full text-blue-700"
+                      className="w-full text-black bg-primary"
                     >
                       Login
                     </Button>
                   </Link>
 
                   <Link href={"/auth/register"}>
-                    <Button variant={"outline"} className="w-full ">
+                    <Button variant={"outline"} className="w-full text-primary bg-black ">
                       Sign Up
                     </Button>
                   </Link>
